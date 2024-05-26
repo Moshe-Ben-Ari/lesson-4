c3ecc86ca4a23bd3b4e095582bb2ace9446535dc
Moshe-Ben-Ari <moshe.benari@gmail.com>
2024-05-26 11:15:25 +0300

    Add project files.

---
+++ lesson 4/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Loops
+            While condition allows for a loop based on as long as a condition =true it loops.
+            while (score !=-1) will loop the statement as long as the 'score' input value isnt -1, and once -1 is put in, the loops breaks.
+            X++ will add 1 to X. Y += X is the same as Y = Y + X. 
+            while (X != -1) {
+                Y++;
+            `   Z += X;
+
+                Console.WriteLine(Enter X (-1 ends loop))
+                X = int.Parse(Console.ReadLine())
+             }
+            if (Y > 0)
+            Console.WriteLine(((double) Z / Y ));
+
+            this code takes an numerous inputs and averages them.
+
+            int Y = X
+            int Z = X 
+            ....
+
+            if (Y < X)
+                Y = X
+            if (Z > X)
+                Z = X
+            this calculates the minimum and maximum value of X 
+            ------------------------------------------------------
+            '\t' produces a tab  in Console.WriteLine
+            
+            with loops you need to remember to
+            1. define the variables to be used
+            2. decide what value will be the conditional value
+            3. what needs to be outside the loop
+            4. always change the value of the conditional value to avoid infinit loops
+            
+            you can use an if with a break statement to end a loop
+
+            while (condition1)
+                    statement1
+                if (condition2) break
+                    statement2
+
+            so one can do the following
+            while (true){
+                read
+                if ("break condition") break;
+                process
+                }
+            this allows the read part pf the read-process loop to not need to be called before the loop and therefore is not written twice.
+            this can also be used to allow multiple exit conditions, one in the while, and on in the 'if () break'
+
+             */
+
+        }
+    }
+}
